3ebe6df056a8eeb1aae014ea694eafb04bebdea4
Matthieu Dorier <mdorier@anl.gov>
2025-12-22 13:14:45 +0000

    added octopus 0.1.1

--- spack_repo/diaspora/packages/diaspora_stream_octopus/package.py
+++ spack_repo/diaspora/packages/diaspora_stream_octopus/package.py
@@ -33,6 +33,7 @@ class DiasporaStreamOctopus(CMakePackage):
     maintainers("mdorier")
 
     version("main", branch="main")
+    version("0.1.1", sha256="660b4dc91e3c3a176c4cca2776a5e8ec2b30fe6a7c6a4fca1657713b5b8c8a7e")
     version("0.1.0", sha256="a87d82f23ad3d5d68577ee0d243bfbd0f82d4089aa0a19e42b7cd6579a60ca13")
 
     depends_on("c", type="build")
